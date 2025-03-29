f61e4c9ed6b68c320ff17297d362d6a8067036a1
Rare Pasec <dhillonaaspreet@gmail.com>
2025-03-28 20:15:05 -0700

    worked on authentication and creating the basic authentication pages

--- client/src/app/auth/signup/page.tsx
+++ client/src/app/auth/signup/page.tsx
@@ -1,17 +1,37 @@
 "use client";
 
+import SelectUsername from "@/app/components/modals/usernameSelection";
+import { auth, provider } from "@/app/config/firebase";
 import { GoogleLogo } from "@phosphor-icons/react";
+import { signInWithPopup } from "firebase/auth";
+import { useRouter } from "next/navigation";
 import React from "react";
 import toast from "react-hot-toast";
 
 function SignUp() {
-  const handleGoogleAuth = () => {};
+  const router = useRouter();
+
+  const handleGoogleAuth = async () => {
+    try {
+      await toast
+        .promise(signInWithPopup(auth, provider), {
+          loading: "Loading...",
+          success: "Login Success!",
+          error: "Login Failed...",
+        })
+        .then(() => {
+          router.push("/");
+        });
+    } catch (error) {}
+  };
+
   return (
     <div>
+      <SelectUsername />
       <div className="flex justify-center font-bold text-2xl mt-40">SignUp</div>
       <div className="flex flex-row justify-center mt-4">
-        <div className="flex items-center text-xl font-medium gap-3 rounded-block" onClick={handleGoogleAuth}>
-          <GoogleLogo size={32} color="var(--beaver)" />
+        <div className="flex items-center text-xl font-medium gap-3 rd-block hover:bg-antiqueWhite" onClick={handleGoogleAuth}>
+          <GoogleLogo size={32} color="var(--color-beaver)" />
           continue with google
         </div>
       </div>
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${nunito.className} antialiased`}>{children}</body>
+      <body className={`${nunito.className} antialiased`}>
+        {children}
+        <Toaster position="bottom-center" />
+      </body>
     </html>
   );
 }
